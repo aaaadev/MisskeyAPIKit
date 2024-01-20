29844cc0ace30e09cc3828b4a74f557990b89bf7
Yeri Kim <laplacianmatrix@gmail.com>
2024-01-20 19:36:24 +0900

    chore: modify `Notification`

--- Sources/MisskeyAPIKit/Types/Notification.swift
+++ Sources/MisskeyAPIKit/Types/Notification.swift
@@ -1,9 +1,27 @@
 public struct Notification: Codable {
-    public let id: String?
-    public let createdAt: String?
-    // public let type: ActionType?
+    public enum ActionType: String, Codable {
+        case follow
+        case mention
+        case reply
+        case renote
+        case quote
+        case reaction
+        case pollVote
+        case pollEnded
+        case receiveFollowRequest
+        case followRequestAccepted
+        case groupInvited
+        case app
+    }
+    public let id: String
+    public let createdAt: String
+    public let type: ActionType
+    public let isRead: Bool
     public let userId: String?
-    public let user: User?
+    public let user: User
     public let reaction: String?
-    public let note: Note?
+    public let note: Note
+    public let body: String?
+    public let header: String?
+    public let icon: String?
 }
